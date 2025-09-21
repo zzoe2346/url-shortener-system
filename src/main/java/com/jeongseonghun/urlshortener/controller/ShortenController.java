7796a93cd5f67d2666c4ddf9a4064c533ce13e01
Seonghun Jeong <tjdgns5506@gmail.com>
2025-09-21 17:42:10 +0900

    feat: URL Shorten API의 ShortenRequest DTO에 Valid 로직 추가

--- src/main/java/com/jeongseonghun/urlshortener/controller/ShortenController.java
+++ src/main/java/com/jeongseonghun/urlshortener/controller/ShortenController.java
@@ -3,6 +3,7 @@ package com.jeongseonghun.urlshortener.controller;
 import com.jeongseonghun.urlshortener.dto.ShortenRequest;
 import com.jeongseonghun.urlshortener.dto.ShortenResponse;
 import com.jeongseonghun.urlshortener.service.ShortenService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,7 @@ public class ShortenController {
     }
 
     @PostMapping("/urls")
-    public ResponseEntity<ShortenResponse> shorten(@RequestBody ShortenRequest request) {
+    public ResponseEntity<ShortenResponse> shorten(@Valid @RequestBody ShortenRequest request) {
         String shortenUrl = shortenService.shortenUrl(request.originalUrl());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ShortenResponse(shortenUrl));
--- src/main/java/com/jeongseonghun/urlshortener/dto/ShortenRequest.java
+++ src/main/java/com/jeongseonghun/urlshortener/dto/ShortenRequest.java
@@ -1,4 +1,14 @@
 package com.jeongseonghun.urlshortener.dto;
 
-public record ShortenRequest(String originalUrl) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record ShortenRequest(
+        @NotBlank(message = "URL은 비어 있을 수 없습니다.")
+        @Pattern(
+                regexp = "^(https?://).+",
+                message = "URL은 http:// 또는 https:// 로 시작해야 합니다."
+        )
+        String originalUrl
+) {
 }
