61de78bef86039b6d9ee384041e509b6609acd87
Seonghun Jeong <tjdgns5506@gmail.com>
2025-09-21 17:40:43 +0900

    feat: URL 엔티티의 ID 필드 ID 생성 전략 GenerationType.IDENTITY로 설정

--- src/main/java/com/jeongseonghun/urlshortener/domain/entity/URL.java
+++ src/main/java/com/jeongseonghun/urlshortener/domain/entity/URL.java
@@ -1,8 +1,6 @@
 package com.jeongseonghun.urlshortener.domain.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class URL {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, unique = true)
     private String originalUrl;
