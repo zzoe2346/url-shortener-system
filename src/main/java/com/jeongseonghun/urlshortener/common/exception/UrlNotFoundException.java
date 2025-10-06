68e850855f37370136bfb3d0a82812f9cb2c18b0
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-06 21:08:04 +0900

    chore: exception 패키지 위치 변경

--- src/main/java/com/jeongseonghun/urlshortener/common/exception/UrlNotFoundException.java
+++ src/main/java/com/jeongseonghun/urlshortener/common/exception/UrlNotFoundException.java
@@ -1,4 +1,4 @@
-package com.jeongseonghun.urlshortener.exception;
+package com.jeongseonghun.urlshortener.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
