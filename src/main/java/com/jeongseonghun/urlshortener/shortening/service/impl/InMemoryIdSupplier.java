4034e53447b139af6db2e608160192e19dc844c1
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-08 01:17:06 +0900

    refactor: rename NonDistributedIdSupplier to InMemoryIdSupplier

--- src/main/java/com/jeongseonghun/urlshortener/shortening/service/impl/InMemoryIdSupplier.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/service/impl/InMemoryIdSupplier.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Component
-public class NonDistributedIdSupplier implements IdSupplier {
+public class InMemoryIdSupplier implements IdSupplier {
 
     private static final AtomicLong atomicLong = new AtomicLong(1);
 
