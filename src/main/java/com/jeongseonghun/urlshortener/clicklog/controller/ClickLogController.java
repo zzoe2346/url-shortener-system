96ddab103ab4f5a324f473bad67c55d66cc49637
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-08 17:41:50 +0900

    chore: 구현 할 예정이 없어져서 ClickLogController 클래스 제거

--- src/main/java/com/jeongseonghun/urlshortener/clicklog/controller/ClickLogController.java
+++
@@ -1,7 +0,0 @@
-package com.jeongseonghun.urlshortener.clicklog.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ClickLogController {
-}
