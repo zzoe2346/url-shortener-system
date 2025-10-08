1f4dc524fcbb2bb973a1172164072048de680073
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-08 17:41:10 +0900

    feat: shortening and redirection을 대상으로한Validation Handler 인터페이스 및 구현체 추가. Chain-Of-Responsibility 패턴 도입.

--- src/main/java/com/jeongseonghun/urlshortener/shortening/service/impl/ShorteningServiceImpl.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/service/impl/ShorteningServiceImpl.java
@@ -1,13 +1,15 @@
 package com.jeongseonghun.urlshortener.shortening.service.impl;
 
 import com.jeongseonghun.urlshortener.clicklog.service.ClickLogService;
+import com.jeongseonghun.urlshortener.common.exception.UrlNotFoundException;
+import com.jeongseonghun.urlshortener.common.util.Base62;
 import com.jeongseonghun.urlshortener.shortening.model.entity.UrlMapping;
 import com.jeongseonghun.urlshortener.shortening.repository.UrlMappingRepository;
-import com.jeongseonghun.urlshortener.common.exception.UrlNotFoundException;
 import com.jeongseonghun.urlshortener.shortening.service.IdSupplier;
 import com.jeongseonghun.urlshortener.shortening.service.ShorteningService;
-import com.jeongseonghun.urlshortener.common.util.Base62;
+import com.jeongseonghun.urlshortener.shortening.validator.ValidationHandler;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,14 +18,24 @@ public class ShorteningServiceImpl implements ShorteningService {
     private final UrlMappingRepository urlMappingRepository;
     private final IdSupplier idSupplier;
     private final ClickLogService clickLogService;
+    private final ValidationHandler shortenChain;
+    private final ValidationHandler redirectChain;
 
-    public ShorteningServiceImpl(UrlMappingRepository urlMappingRepository, IdSupplier idSupplier, ClickLogService clickLogService) {
+    public ShorteningServiceImpl(UrlMappingRepository urlMappingRepository,
+                                 IdSupplier idSupplier,
+                                 ClickLogService clickLogService,
+                                 @Qualifier("shortenValidationChain") ValidationHandler shortenChain,
+                                 @Qualifier("redirectValidationChain") ValidationHandler redirectChain) {
         this.urlMappingRepository = urlMappingRepository;
         this.idSupplier = idSupplier;
         this.clickLogService = clickLogService;
+        this.shortenChain = shortenChain;
+        this.redirectChain = redirectChain;
     }
 
     public String getOrCreateShortUrl(String originalUrl) {
+        shortenChain.validate(originalUrl);
+
         UrlMapping urlMapping = urlMappingRepository.findByOriginalUrl(originalUrl)
                 .orElseGet(() -> {
                             String shortCode = Base62.Encoder.encode(idSupplier.getId());
@@ -34,6 +46,8 @@ public class ShorteningServiceImpl implements ShorteningService {
     }
 
     public String getOriginalUrl(String shortCode, HttpServletRequest request) {
+        redirectChain.validate(shortCode, request);
+
         UrlMapping urlMapping = urlMappingRepository.findByShortCode(shortCode)
                 .orElseThrow(() -> new UrlNotFoundException("해당 단축 코드를 찾을 수 없습니다: " + shortCode));
 
---
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/validator/impl/UrlFormatHandler.java
@@ -0,0 +1,27 @@
+package com.jeongseonghun.urlshortener.shortening.validator.impl;
+
+import com.jeongseonghun.urlshortener.shortening.validator.ValidationHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ValidationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UrlFormatHandler implements ValidationHandler {
+
+    private ValidationHandler next;
+
+    @Override
+    public void setNext(ValidationHandler next) {
+        this.next = next;
+    }
+
+    @Override
+    public void validate(String originalUrl) throws ValidationException {
+
+    }
+
+    @Override
+    public void validate(String shortCode, HttpServletRequest request) throws ValidationException {
+
+    }
+}
