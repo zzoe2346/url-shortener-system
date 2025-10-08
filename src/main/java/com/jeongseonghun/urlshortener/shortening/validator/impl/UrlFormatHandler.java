253b891b8c329aae65e9fc876d8bdd7be350a9b3
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-08 17:50:58 +0900

    feat: UrlFormatHandler에 URL 유효성 검증 로직 추가

--- src/main/java/com/jeongseonghun/urlshortener/shortening/validator/impl/UrlFormatHandler.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/validator/impl/UrlFormatHandler.java
@@ -17,11 +17,19 @@ public class UrlFormatHandler implements ValidationHandler {
 
     @Override
     public void validate(String originalUrl) throws ValidationException {
-
+        if (originalUrl.isEmpty()) {
+            throw new ValidationException("URL이 없습니다.");
+        }
+        if (!originalUrl.matches("^(https?://).+")) {
+            throw new ValidationException("URL은 http:// 또는 https:// 로 시작해야 합니다.");
+        }
+        if (next != null) {
+            next.validate(originalUrl);
+        }
     }
 
     @Override
     public void validate(String shortCode, HttpServletRequest request) throws ValidationException {
-
+        // Redirection용 (사용 안 함)
     }
 }
