cc76040a12ebba801a971a2934c2280e70a89641
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-08 20:04:10 +0900

    feat: CircularShortenHandler에 순환 URL 단축을 막기위한 도메인 검증 로직 추가 및 ShortCodeFormatHandler에 ShortCode 유효성 검증 로직 추가

--- src/main/java/com/jeongseonghun/urlshortener/shortening/validator/impl/CircularShortenHandler.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/validator/impl/CircularShortenHandler.java
@@ -1,14 +1,22 @@
 package com.jeongseonghun.urlshortener.shortening.validator.impl;
 
+import com.jeongseonghun.urlshortener.common.config.AppProperties;
 import com.jeongseonghun.urlshortener.shortening.validator.ValidationHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ValidationException;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
+
 @Component
 public class CircularShortenHandler implements ValidationHandler {
 
+    private final AppProperties appProperties;
     private ValidationHandler next;
+
+    public CircularShortenHandler(AppProperties appProperties) {
+        this.appProperties = appProperties;
+    }
 
     @Override
     public void setNext(ValidationHandler next) {
@@ -17,11 +25,26 @@ public class CircularShortenHandler implements ValidationHandler {
 
     @Override
     public void validate(String originalUrl) throws ValidationException {
+        if (appProperties.getDomain().equals(extractDomain(originalUrl))) {
+            throw new ValidationException("이미 저희 서비스에서 단축하여 제공되는 URL은 단축이 불가합니다.");
+        }
+        if (next != null) {
+            next.validate(originalUrl);
+        }
+    }
 
+    /**
+     * URL 문자열에서 도메인 부분을 추출하는 메서드.
+     *
+     * @param originalUrl 도메인 추출할 URL 문자열
+     * @return 도메인 문자열
+     */
+    private String extractDomain(String originalUrl) {
+        return URI.create(originalUrl).getHost();
     }
 
     @Override
     public void validate(String shortCode, HttpServletRequest request) throws ValidationException {
-
+        // Redirection용 (사용 안 함)
     }
 }
--- src/main/java/com/jeongseonghun/urlshortener/shortening/validator/impl/ShortCodeFormatHandler.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/validator/impl/ShortCodeFormatHandler.java
@@ -17,11 +17,16 @@ public class ShortCodeFormatHandler implements ValidationHandler {
 
     @Override
     public void validate(String originalUrl) throws ValidationException {
-
+        // Shorten용 (사용 안 함)
     }
 
     @Override
     public void validate(String shortCode, HttpServletRequest request) throws ValidationException {
-
+        if (!shortCode.matches("^[a-zA-Z0-9]+$")) {
+            throw new ValidationException("ShortCode는 영문 대소문자와 숫자로만 구성되어야 합니다.");
+        }
+        if (next != null) {
+            next.validate(shortCode, request);
+        }
     }
 }
