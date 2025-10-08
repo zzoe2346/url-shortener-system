9365a624c2adf1df6bb8d76be3f073e68afca0c1
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-08 17:51:56 +0900

    refactor: Validator 핸들러 도입으로인해 ShortenRequest에서 URL 유효성 검증 로직 제거

--- src/main/java/com/jeongseonghun/urlshortener/shortening/model/dto/ShortenRequest.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/model/dto/ShortenRequest.java
@@ -1,14 +1,4 @@
 package com.jeongseonghun.urlshortener.shortening.model.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-
-public record ShortenRequest(
-        @NotBlank(message = "URL은 비어 있을 수 없습니다.")
-        @Pattern(
-                regexp = "^(https?://).+",
-                message = "URL은 http:// 또는 https:// 로 시작해야 합니다."
-        )
-        String originalUrl
-) {
+public record ShortenRequest(String originalUrl) {
 }
