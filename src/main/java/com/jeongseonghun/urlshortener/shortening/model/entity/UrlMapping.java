d3772183818cf55fccbd87bdf65988e6dc6cd6fb
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-10 22:57:53 +0900

    fix: shortCode의 경우 유일성이 보장된 상태로 생성되기에 DB단에서 unique 세팅 제거

--- src/main/java/com/jeongseonghun/urlshortener/shortening/model/entity/UrlMapping.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/model/entity/UrlMapping.java
@@ -20,7 +20,7 @@ public class UrlMapping {
     private Long id;
     @Column(nullable = false, unique = true)
     private String originalUrl;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String shortCode;
     @CreationTimestamp
     private LocalDateTime createdAt;
