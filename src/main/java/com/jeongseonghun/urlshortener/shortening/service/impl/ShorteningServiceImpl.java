0dd153550a039fbc88f045a7ae3ae2fe98b27834
Seonghun Jeong <tjdgns5506@gmail.com>
2025-10-11 20:41:42 +0900

    feat: ShorteningServiceImpl에 비동기 URL 매핑 저장 및 분산 잠금 기능 추가

--- src/main/java/com/jeongseonghun/urlshortener/shortening/service/impl/ShorteningServiceImpl.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/service/impl/ShorteningServiceImpl.java
@@ -10,8 +10,13 @@ import com.jeongseonghun.urlshortener.shortening.service.IdSupplier;
 import com.jeongseonghun.urlshortener.shortening.service.ShorteningService;
 import com.jeongseonghun.urlshortener.shortening.validator.ValidationHandler;
 import jakarta.servlet.http.HttpServletRequest;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ShorteningServiceImpl implements ShorteningService {
@@ -22,30 +27,54 @@ public class ShorteningServiceImpl implements ShorteningService {
     private final ValidationHandler shortenChain;
     private final ValidationHandler redirectChain;
     private final AppProperties appProperties;
+    private final AsyncUrlMappingService asyncUrlMappingService;
+    private final RedissonClient redissonClient;
+
 
     public ShorteningServiceImpl(UrlMappingRepository urlMappingRepository,
                                  IdSupplier idSupplier,
                                  ClickLogService clickLogService,
                                  @Qualifier("shortenValidationChain") ValidationHandler shortenChain,
-                                 @Qualifier("redirectValidationChain") ValidationHandler redirectChain, AppProperties appProperties) {
+                                 @Qualifier("redirectValidationChain") ValidationHandler redirectChain,
+                                 AppProperties appProperties,
+                                 AsyncUrlMappingService asyncUrlMappingService,
+                                 RedissonClient redissonClient) {
         this.urlMappingRepository = urlMappingRepository;
         this.idSupplier = idSupplier;
         this.clickLogService = clickLogService;
         this.shortenChain = shortenChain;
         this.redirectChain = redirectChain;
         this.appProperties = appProperties;
+        this.asyncUrlMappingService = asyncUrlMappingService;
+        this.redissonClient = redissonClient;
     }
 
     public String getOrCreateShortUrl(String originalUrl) {
         shortenChain.validate(originalUrl);
 
-        UrlMapping urlMapping = urlMappingRepository.findByOriginalUrl(originalUrl)
-                .orElseGet(() -> {
-                            String shortCode = Base62.Encoder.encode(idSupplier.getId());
-                            return urlMappingRepository.save(new UrlMapping(originalUrl, shortCode));
-                        }
-                );
-        return urlMapping.getShortUrl(appProperties.getDomain());
+        Optional<UrlMapping> urlMapping = urlMappingRepository.findByOriginalUrl(originalUrl);
+        if (urlMapping.isPresent()) {
+            return urlMapping.get().getShortUrl(appProperties.getDomain());
+        }
+
+        RLock lock = redissonClient.getLock(originalUrl);
+        try {
+            boolean isLocked = lock.tryLock(3, 1, TimeUnit.SECONDS);
+            if (!isLocked) {
+                throw new RuntimeException("잠시 후 다시 시도해주세요.");
+            }
+
+            String newShortCode = Base62.Encoder.encode(idSupplier.getId());
+            UrlMapping newUrlMapping = new UrlMapping(originalUrl, newShortCode);
+
+            asyncUrlMappingService.saveToDbAsync(originalUrl, newShortCode);
+
+            return newUrlMapping.getShortUrl(appProperties.getDomain());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public String getOriginalUrl(String shortCode, HttpServletRequest request) {
