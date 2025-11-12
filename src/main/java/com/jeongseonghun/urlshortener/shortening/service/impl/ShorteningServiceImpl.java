c9785d4f6f72ac6bb9033e02543f079ea2378dc6
Seonghun Jeong <tjdgns5506@gmail.com>
2025-11-12 19:26:01 +0900

    docs: 비즈니스 로직에 주석 추가. 개행을 주석으로 대체. 좀더 유의미한 코드로 개선 기대.

--- src/main/java/com/jeongseonghun/urlshortener/shortening/service/impl/ShorteningServiceImpl.java
+++ src/main/java/com/jeongseonghun/urlshortener/shortening/service/impl/ShorteningServiceImpl.java
@@ -30,7 +30,6 @@ public class ShorteningServiceImpl implements ShorteningService {
     private final AsyncUrlMappingService asyncUrlMappingService;
     private final RedissonClient redissonClient;
 
-
     public ShorteningServiceImpl(UrlMappingRepository urlMappingRepository,
                                  IdSupplier idSupplier,
                                  ClickLogService clickLogService,
@@ -51,40 +50,42 @@ public class ShorteningServiceImpl implements ShorteningService {
 
     public String getOrCreateShortUrl(String originalUrl) {
         shortenChain.validate(originalUrl);
-
         Optional<UrlMapping> urlMapping = urlMappingRepository.findByOriginalUrl(originalUrl);
+        // 원본 URL과 매핑된 단축 URL이 있는 경우: 매핑된 단축 URL을 리턴
         if (urlMapping.isPresent()) {
             return urlMapping.get().getShortUrl(appProperties.getDomain());
         }
-
+        // 원본 URL과 매핑된 단축 URL이 없는 경우: 단축 URL 새로 생성하고 매핑 시킴
+        // 1. originalUrl 을 이름으로 하는 락을 획득
         RLock lock = redissonClient.getLock(originalUrl);
+
         try {
             boolean isLocked = lock.tryLock(3, 1, TimeUnit.SECONDS);
+            // 2-1. 락 획득에 실패하면 예외를 던짐
             if (!isLocked) {
                 throw new RuntimeException("잠시 후 다시 시도해주세요.");
             }
-
+            // 2-2. 락 획득에 성공한 경우 ID 채번기로부터 ID를 얻어서 Base62 인코딩을 하여 새로운 ShortCode를 생성
             String newShortCode = Base62.Encoder.encode(idSupplier.getId());
+            // 3. DB에 원본 URL - 단축 URL(shortCode) 매핑 되도록 저장
+            // => 비동기 처리
             UrlMapping newUrlMapping = new UrlMapping(originalUrl, newShortCode);
-
             asyncUrlMappingService.saveToDbAsync(originalUrl, newShortCode);
-
+            // 4. 단축 URL 리턴
             return newUrlMapping.getShortUrl(appProperties.getDomain());
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
+            // 락은 꼭 해제할 것!
             lock.unlock();
         }
     }
 
     public String getOriginalUrl(String shortCode, HttpServletRequest request) {
         redirectChain.validate(shortCode, request);
-
         UrlMapping urlMapping = urlMappingRepository.findByShortCode(shortCode)
                 .orElseThrow(() -> new UrlNotFoundException("해당 단축 코드를 찾을 수 없습니다: " + shortCode));
-
         clickLogService.recordClick(request, urlMapping);
-
         return urlMapping.getOriginalUrl();
     }
 }
