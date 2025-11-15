33fa9402315cc3c02c1be664499d65c7931724f8
Seonghun Jeong <tjdgns5506@gmail.com>
2025-11-15 16:25:15 +0900

    feat: ClickLog 엔티티에 명시적 생성자 추가, `@CreationTimestamp` 활용

--- src/main/java/com/jeongseonghun/urlshortener/domain/ClickLog.java
+++ src/main/java/com/jeongseonghun/urlshortener/domain/ClickLog.java
@@ -1,26 +1,35 @@
 package com.jeongseonghun.urlshortener.domain;
 
 import jakarta.persistence.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 public class ClickLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
-    @JoinColumn(name = "url_mapping_id")
+    @JoinColumn(name = "short_url_id")
     private ShortUrl shortUrl;
     private String ipAddress;
     private String userAgent;
     private String referrer;
     private String acceptLanguage;
+    @CreationTimestamp
     private LocalDateTime clickedAt;
+
+    public ClickLog(ShortUrl shortUrl, HttpServletRequest request) {
+        this.shortUrl = shortUrl;
+        this.ipAddress = request.getHeader("X-Forwarded-For");
+        this.userAgent = request.getHeader("User-Agent");
+        this.referrer = request.getHeader("Referer");
+        this.acceptLanguage = request.getHeader("Accept-Language");
+    }
 }
