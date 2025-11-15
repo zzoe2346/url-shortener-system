8924dbc7abe99ca180f7c8f4fd024d621b7d08ae
Seonghun Jeong <tjdgns5506@gmail.com>
2025-11-15 18:53:28 +0900

    fix: INVALID_URL_FORMAT 메시지 수정

--- src/main/java/com/jeongseonghun/urlshortener/support/Message.java
+++ src/main/java/com/jeongseonghun/urlshortener/support/Message.java
@@ -1,7 +1,7 @@
 package com.jeongseonghun.urlshortener.support;
 
 public class Message {
-    public static final String INVALID_URL_FORMAT = "URL은 http:// 또는 https:// 로 시작해야 합니다.";
+    public static final String INVALID_URL_FORMAT = "유효하지 않은 URL 포맷입니다.";
     public static final String SHORTKEY_NOT_FOUND = "해당 ShortKey를 찾을 수 없습니다.";
     public static final String INVALID_SHORTKEY = "ShortKey는 영문 대소문자와 숫자로만 구성되어야 합니다.";
     public static final String DUPLICATE_URL = "이미 저희 서비스에서 단축하여 제공되는 URL은 단축이 불가합니다.";
