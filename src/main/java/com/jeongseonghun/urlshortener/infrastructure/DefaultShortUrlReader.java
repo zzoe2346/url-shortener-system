c060a7eb9889f7827190c10e1edaa76bb3a6f1a2
Seonghun Jeong <tjdgns5506@gmail.com>
2025-11-15 15:14:22 +0900

    feat: ShortUrlReader 구현체 DefaultShortUrlReader 추가

---
+++ src/main/java/com/jeongseonghun/urlshortener/infrastructure/DefaultShortUrlReader.java
@@ -0,0 +1,25 @@
+package com.jeongseonghun.urlshortener.infrastructure;
+
+import com.jeongseonghun.urlshortener.domain.OriginalUrl;
+import com.jeongseonghun.urlshortener.domain.ShortUrl;
+import com.jeongseonghun.urlshortener.domain.ShortUrlReader;
+import com.jeongseonghun.urlshortener.repository.ShortUrlRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DefaultShortUrlReader implements ShortUrlReader {
+
+    private final ShortUrlRepository shortUrlRepository;
+
+    public DefaultShortUrlReader(ShortUrlRepository shortUrlRepository) {
+        this.shortUrlRepository = shortUrlRepository;
+    }
+
+    @Override
+    public Optional<ShortUrl> findShortUrl(OriginalUrl originalUrl) {
+        return shortUrlRepository.findByOriginalUrl(originalUrl.value());
+    }
+
+}
