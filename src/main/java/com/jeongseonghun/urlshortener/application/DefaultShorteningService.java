431e32461358824a0256c17db3c65a875bef9b23
Seonghun Jeong <tjdgns5506@gmail.com>
2025-11-15 15:20:56 +0900

    refactor: DefaultShorteningService 대대적 리팩토링 -  `@RequiredArgsConstructor` 도입하여 생성자 코드 생략, 미사용 메서드와 임포트 제거, validate 제거(validate 추후 재 도입 예정)

--- src/main/java/com/jeongseonghun/urlshortener/application/DefaultShorteningService.java
+++ src/main/java/com/jeongseonghun/urlshortener/application/DefaultShorteningService.java
@@ -1,62 +1,36 @@
 package com.jeongseonghun.urlshortener.application;
 
-import com.jeongseonghun.urlshortener.config.AppProperties;
+import com.jeongseonghun.urlshortener.api.dto.ShortenResponse;
 import com.jeongseonghun.urlshortener.domain.*;
-import com.jeongseonghun.urlshortener.domain.UrlNotFoundException;
-import com.jeongseonghun.urlshortener.infrastructure.AsyncShortUrlWriter;
-import com.jeongseonghun.urlshortener.support.Base62;
 import com.jeongseonghun.urlshortener.repository.ShortUrlRepository;
-import com.jeongseonghun.urlshortener.domain.ValidationHandler;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@RequiredArgsConstructor
 public class DefaultShorteningService implements ShorteningService {
 
     private final ShortUrlRepository shortUrlRepository;
-    private final IdSupplier idSupplier;
     private final ClickLogWriter clickLogWriter;
-    private final ValidationHandler shortenChain;
-    private final ValidationHandler redirectChain;
-    private final AppProperties appProperties;
-    private final AsyncShortUrlWriter asyncUrlMappingService;
+    private final ShortUrlWriter shortUrlWriter;
     private final RedissonClient redissonClient;
+    private final ShortUrlReader shortUrlReader;
+    private final ShortKeyGenerator shortKeyGenerator;
 
-    public DefaultShorteningService(ShortUrlRepository shortUrlRepository,
-                                    IdSupplier idSupplier,
-                                    ClickLogWriter clickLogWriter,
-                                    @Qualifier("shortenValidationChain") ValidationHandler shortenChain,
-                                    @Qualifier("redirectValidationChain") ValidationHandler redirectChain,
-                                    AppProperties appProperties,
-                                    AsyncShortUrlWriter asyncUrlMappingService,
-                                    RedissonClient redissonClient) {
-        this.shortUrlRepository = shortUrlRepository;
-        this.idSupplier = idSupplier;
-        this.clickLogWriter = clickLogWriter;
-        this.shortenChain = shortenChain;
-        this.redirectChain = redirectChain;
-        this.appProperties = appProperties;
-        this.asyncUrlMappingService = asyncUrlMappingService;
-        this.redissonClient = redissonClient;
-    }
-
-    public String getOrCreateShortUrl(String originalUrl) {
-        shortenChain.validate(originalUrl);
-        Optional<ShortUrl> urlMapping = shortUrlRepository.findByOriginalUrl(originalUrl);
-        // 원본 URL과 매핑된 단축 URL이 있는 경우: 매핑된 단축 URL을 리턴
-        if (urlMapping.isPresent()) {
-            return urlMapping.get().getShortUrl(appProperties.getDomain());
+    public ShortenResponse getOrCreateShortUrl(String rawUrl) {
+        OriginalUrl originalUrl = OriginalUrl.of(rawUrl);
+        Optional<ShortUrl> existingShortUrl = shortUrlReader.findShortUrl(originalUrl);
+        if (existingShortUrl.isPresent()) {
+            return ShortenResponse.from(existingShortUrl.get());
         }
-        // 원본 URL과 매핑된 단축 URL이 없는 경우: 단축 URL 새로 생성하고 매핑 시킴
         // 1. originalUrl 을 이름으로 하는 락을 획득
-        RLock lock = redissonClient.getLock(originalUrl);
-
+        RLock lock = redissonClient.getLock(originalUrl.getValue());
         try {
             boolean isLocked = lock.tryLock(3, 1, TimeUnit.SECONDS);
             // 2-1. 락 획득에 실패하면 예외를 던짐
@@ -64,13 +38,12 @@ public class DefaultShorteningService implements ShorteningService {
                 throw new RuntimeException("잠시 후 다시 시도해주세요.");
             }
             // 2-2. 락 획득에 성공한 경우 ID 채번기로부터 ID를 얻어서 Base62 인코딩을 하여 새로운 ShortCode를 생성
-            String newShortCode = Base62.Encoder.encode(idSupplier.getId());
             // 3. DB에 원본 URL - 단축 URL(shortCode) 매핑 되도록 저장
             // => 비동기 처리
-            ShortUrl newShortUrl = new ShortUrl(originalUrl, newShortCode);
-            asyncUrlMappingService.saveToDbAsync(originalUrl, newShortCode);
+            ShortUrl newShortUrl = new ShortUrl(originalUrl, shortKeyGenerator.generateShortKey());
+            shortUrlWriter.saveToDbAsync(newShortUrl);
             // 4. 단축 URL 리턴
-            return newShortUrl.getShortUrl(appProperties.getDomain());
+            return ShortenResponse.from(newShortUrl);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
@@ -80,10 +53,9 @@ public class DefaultShorteningService implements ShorteningService {
     }
 
     public String getOriginalUrl(String shortCode, HttpServletRequest request) {
-        redirectChain.validate(shortCode, request);
         ShortUrl shortUrl = shortUrlRepository.findByShortCode(shortCode)
                 .orElseThrow(() -> new UrlNotFoundException("해당 단축 코드를 찾을 수 없습니다: " + shortCode));
         clickLogWriter.recordClick(request, shortUrl);
-        return shortUrl.getOriginalUrl();
+        return shortUrl.getOriginalUrl().getValue();
     }
 }
