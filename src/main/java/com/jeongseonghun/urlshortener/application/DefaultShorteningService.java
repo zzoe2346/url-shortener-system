326a78e900e3f4c5298a9269abc6ae531fdebd4a
Seonghun Jeong <tjdgns5506@gmail.com>
2025-11-15 16:33:32 +0900

    docs: DefaultShorteningService에 있던 주석 모두 제거

--- src/main/java/com/jeongseonghun/urlshortener/application/DefaultShorteningService.java
+++ src/main/java/com/jeongseonghun/urlshortener/application/DefaultShorteningService.java
@@ -27,25 +27,18 @@ public class DefaultShorteningService implements ShorteningService {
         if (existingShortUrl.isPresent()) {
             return ShortenResponse.from(existingShortUrl.get());
         }
-        // 1. originalUrl 을 이름으로 하는 락을 획득
         RLock lock = redissonClient.getLock(originalUrl.getValue());
         try {
             boolean isLocked = lock.tryLock(3, 1, TimeUnit.SECONDS);
-            // 2-1. 락 획득에 실패하면 예외를 던짐
             if (!isLocked) {
                 throw new RuntimeException("잠시 후 다시 시도해주세요.");
             }
-            // 2-2. 락 획득에 성공한 경우 ID 채번기로부터 ID를 얻어서 Base62 인코딩을 하여 새로운 ShortCode를 생성
-            // 3. DB에 원본 URL - 단축 URL(shortCode) 매핑 되도록 저장
-            // => 비동기 처리
             ShortUrl newShortUrl = new ShortUrl(originalUrl, shortKeyGenerator.generateShortKey());
             shortUrlWriter.saveToDbAsync(newShortUrl);
-            // 4. 단축 URL 리턴
             return ShortenResponse.from(newShortUrl);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
-            // 락은 꼭 해제할 것!
             lock.unlock();
         }
     }
