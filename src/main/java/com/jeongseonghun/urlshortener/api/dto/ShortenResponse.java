d5a24cc9f5046e7284fe1d875e0863aa963441a0
Seonghun Jeong <tjdgns5506@gmail.com>
2025-11-15 14:29:46 +0900

    refactor: ShortenResponse 클래스에 ShortUrl로부터 생성하는 정적 메서드 추가

--- src/main/java/com/jeongseonghun/urlshortener/api/dto/ShortenResponse.java
+++ src/main/java/com/jeongseonghun/urlshortener/api/dto/ShortenResponse.java
@@ -1,4 +1,10 @@
 package com.jeongseonghun.urlshortener.api.dto;
 
+import com.jeongseonghun.urlshortener.domain.ShortUrl;
+
 public record ShortenResponse(String shortUrl) {
+    public static ShortenResponse from(ShortUrl shortUrl) {
+        //TODO getShortUrl 할때 파라미터 제거 작업 필요
+        return new ShortenResponse(shortUrl.getShortUrl("temp"));
+    }
 }
