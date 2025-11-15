3cee5450759fa61a3ee8c78933cdabed6189c1fd
Seonghun Jeong <tjdgns5506@gmail.com>
2025-11-15 16:29:39 +0900

    refactor: DefaultClickLogWriter 코드 간결하게 개선. ClickLog 생성은 setter가 아닌 생성자를 이용하도록 이전 커밋에서 개선함

--- src/main/java/com/jeongseonghun/urlshortener/infrastructure/DefaultClickLogWriter.java
+++ src/main/java/com/jeongseonghun/urlshortener/infrastructure/DefaultClickLogWriter.java
@@ -2,14 +2,10 @@ package com.jeongseonghun.urlshortener.infrastructure;
 
 import com.jeongseonghun.urlshortener.domain.ClickLog;
 import com.jeongseonghun.urlshortener.domain.ClickLogWriter;
-import com.jeongseonghun.urlshortener.domain.ShortUrl;
 import com.jeongseonghun.urlshortener.repository.ClickLogRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-
-@Service
+@Component
 public class DefaultClickLogWriter implements ClickLogWriter {
     private final ClickLogRepository clickLogRepository;
 
@@ -18,14 +14,8 @@ public class DefaultClickLogWriter implements ClickLogWriter {
     }
 
     @Override
-    public void recordClick(HttpServletRequest request, ShortUrl shortUrl) {
-        ClickLog clickLog = new ClickLog();
-        clickLog.setShortUrl(shortUrl);
-        clickLog.setIpAddress(request.getHeader("X-Forwarded-For"));
-        clickLog.setUserAgent(request.getHeader("User-Agent"));
-        clickLog.setReferrer(request.getHeader("Referer"));
-        clickLog.setAcceptLanguage(request.getHeader("Accept-Language"));
-        clickLog.setClickedAt(LocalDateTime.now());
+    public void save(ClickLog clickLog) {
         clickLogRepository.save(clickLog);
     }
+
 }
