d545dc244b69c61d053401407778e3ff0e59d01c
Seonghun Jeong <tjdgns5506@gmail.com>
2025-12-08 12:31:07 +0900

    refactor: OriginalUrl 클래스 코드 가독성 개선(중복 기능 코드 통합, Java에서 제공하는 기능 활용)

--- src/main/java/com/jeongseonghun/urlshortener/domain/OriginalUrl.java
+++ src/main/java/com/jeongseonghun/urlshortener/domain/OriginalUrl.java
@@ -4,40 +4,32 @@ import com.jeongseonghun.urlshortener.support.Message;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.web.util.InvalidUrlException;
 
+import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.net.URL;
 
 @Embeddable
 @NoArgsConstructor
 @Getter
 public class OriginalUrl {
 
-    private String value;
+    private URL url;
 
-    //TODO 도메인 얻는 로직이 좀 비효율적으로 보임. 이 클래스 초기화 시점에서 도메인 세팅하는 걸로 변경 고려.
     private OriginalUrl(String value) {
-        this.validate(value);
-        this.value = value.trim();
+        try {
+            this.url = URI.create(value).toURL();
+        } catch (MalformedURLException e) {
+            throw new ValidationException(Message.INVALID_URL_FORMAT);
+        }
     }
 
     public static OriginalUrl of(String value) {
         return new OriginalUrl(value);
     }
 
-    private void validate(String value) {
-        if (!value.matches("^(https?://).+")) {
-            throw new ValidationException(Message.INVALID_URL_FORMAT);
-        }
-    }
-
     public String getDomain() {
-        try {
-            return new URI(this.value).getHost();
-        } catch (URISyntaxException e) {
-            throw new InvalidUrlException(Message.INVALID_URL_FORMAT);
-        }
+        return this.url.getHost();
     }
 
 }
