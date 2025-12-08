5ff7a0e155625fb42d6a1100ab0706c692babdab
Seonghun Jeong <tjdgns5506@gmail.com>
2025-12-08 12:41:37 +0900

    fix: DefaultShorteningService#getOrCreateShortUrl의 분산 락 로직 제거
    - 동일한 OriginalURL이라도 요청마다 새로운 URL 단축키 만드는 방식으로 비즈니스 정책 수정
    - 동시에 같은 OriginalURL 생성을 요청할 경우를 대비하기위한 분산락 로직이었음. 정책 수정으로 더이상 불필요하기에 삭제

--- src/main/java/com/jeongseonghun/urlshortener/application/DefaultShorteningService.java
+++ src/main/java/com/jeongseonghun/urlshortener/application/DefaultShorteningService.java
@@ -4,28 +4,21 @@ import com.jeongseonghun.urlshortener.api.dto.ShortUrlResponse;
 import com.jeongseonghun.urlshortener.config.AppProperties;
 import com.jeongseonghun.urlshortener.domain.*;
 import com.jeongseonghun.urlshortener.infrastructure.ShortUrlCreationPolicyManager;
-import com.jeongseonghun.urlshortener.support.Message;
 import lombok.RequiredArgsConstructor;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class DefaultShorteningService implements ShorteningService {
 
     private final ShortUrlWriter shortUrlWriter;
-    private final RedissonClient redissonClient;
     private final ShortUrlReader shortUrlReader;
     private final ShortKeyGenerator shortKeyGenerator;
     private final AppProperties appProperties;
     private final ShortUrlCreationPolicyManager shortUrlCreationPolicyManager;
-
-    private static final long WAIT_TIME = 2;
 
     @Override
     @Transactional
@@ -36,22 +29,9 @@ public class DefaultShorteningService implements ShorteningService {
         if (existingShortUrl.isPresent()) {
             return ShortUrlResponse.from(existingShortUrl.get(), appProperties.getDomain());
         }
-        RLock lock = redissonClient.getLock(originalUrl.getValue());
-        try {
-            boolean isLocked = lock.tryLock(WAIT_TIME, TimeUnit.SECONDS);
-            if (!isLocked) {
-                throw new RuntimeException(Message.RETRY_LATER);
-            }
-            ShortUrl newShortUrl = new ShortUrl(originalUrl, shortKeyGenerator.generateShortKey());
-            shortUrlWriter.save(newShortUrl);
-            return ShortUrlResponse.from(newShortUrl, appProperties.getDomain());
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (lock.isHeldByCurrentThread()) {
-                lock.unlock();
-            }
-        }
+        ShortUrl newShortUrl = new ShortUrl(originalUrl, shortKeyGenerator.generateShortKey());
+        shortUrlWriter.save(newShortUrl);
+        return ShortUrlResponse.from(newShortUrl, appProperties.getDomain());
     }
 
 }
